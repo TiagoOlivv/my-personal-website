83974b0d19ff2bc94bfd87c124bf55e87047c835
tiagoolivv <suubmundo@gmail.com>
2020-02-08 01:00:11 -0300

    add icon social media

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -11,8 +12,36 @@ export default () => {
 				description="front-end developer, developing apps"
 				keywords="ReactJS, React Native, Node, Gatsby, Front-end, API"
 			/>
-			<h1 className="title has-text-centered">Tiago Oliveira</h1>
-			<h2 className="subtitle has-text-centered">Front-end Developer</h2>
+			<Helmet>
+				<script
+					src="https://kit.fontawesome.com/bf7d5cedf2.js"
+					crossorigin="anonymous"
+				></script>
+			</Helmet>
+			<div className="has-text-centered">
+				<h1 className="title">Tiago Oliveira</h1>
+				<h2 className="subtitle">Front-end Developer</h2>
+				<a href="https://instagram.com/tiagoolivv" aria-label="instagram">
+					<span className="icon is-large">
+						<i class="fab fa-2x fa-instagram" />
+					</span>
+				</a>
+				<a href="https://github.com/tiagoolivv" aria-label="github">
+					<span className="icon is-large">
+						<i className="fab fa-2x fa-github" />
+					</span>
+				</a>
+				<a href="https://t.me/tiagoolivv" aria-label="telegram">
+					<span className="icon is-large">
+						<i class="fab fa-2x fa-telegram" />
+					</span>
+				</a>
+				<a href="https://www.linkedin.com/in/tiagoolivv" aria-label="linkedin">
+					<span className="icon is-large">
+						<i class="fab fa-2x fa-linkedin" />
+					</span>
+				</a>
+			</div>
 			<section className="section content is-size-4-desktop is-size-5-touch">
 				<h4 className="title">Hello, I'm Tiago Oliveira</h4>
 				<p>
