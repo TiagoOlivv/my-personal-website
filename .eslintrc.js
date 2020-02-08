4e504810c94a37d3006adcf112c52cfa4e9419ed
tiagoolivv <suubmundo@gmail.com>
2020-02-07 23:03:26 -0300

    add bulma

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,26 @@
 module.exports = {
-	extends: 'airbnb',
-	parser: 'babel-eslint',
-	settings: {
-		'import/resolver': {
-			node: {
-				paths: ['src'],
-			},
-		},
-	},
-	rules: {
-		'max-len': [
-			'error',
-			{
-				code: 80,
-				ignoreUrls: true,
-				ignoreStrings: true,
-				ignoreTemplateLiterals: true,
-			},
-		],
-		'react/jsx-filename-extension': [
-			1,
-			{
-				extensions: ['.js', '.jsx'],
-			},
-		],
-		'react/forbid-prop-types': 'off',
-	},
-}
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
